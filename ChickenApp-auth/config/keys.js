8281a78700dc7078fe8d9fbecb3890403417dd6c
dwight <dromero551@gmail.com>
2020-12-13 16:50:29 -0500

    Delete keys.js

--- ChickenApp-auth/config/keys.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  mongoURI:
-    "mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@chickenapp.b33m3.mongodb.net/auth-db?retryWrites=true&w=majority",
-  secretOrKey: "secret",
-  userRoles: ["user", "manager"],
-};
