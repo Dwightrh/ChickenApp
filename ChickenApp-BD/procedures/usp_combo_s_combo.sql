a7535caed394f88710940f83ea2dfdb13154aa98
dwight <dromero551@gmail.com>
2020-12-13 16:51:53 -0500

    Delete usp_combo_s_combo.sql

--- ChickenApp-BD/procedures/usp_combo_s_combo.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`admin`@`%` PROCEDURE `usp_combo_s_combo`()
-BEGIN
-	IF (SELECT EXISTS(SELECT * FROM Combo)) THEN
-		SELECT idCombos AS "idCombo" , nombreCombo AS "name" , descriptionCombo AS "description" ,
-		typeCombo AS "type" 
-		FROM Combo ;
-    END IF;
-END
