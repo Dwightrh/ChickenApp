5d000c129140ab390524207838ceec91a5085eea
dwight <dromero551@gmail.com>
2020-12-13 16:58:00 -0500

    Delete client-get-response.model.ts

--- chickenAppFront/src/app/client/models/client-get-response.model.ts
+++
@@ -1,8 +0,0 @@
-// modelo segun la respuesta que envia el backend por GET
-export class ClientGetResponse {
-    status: boolean ;
-    statusCode: number ;
-    message: string ;
-    client: any ;
-  }
-  
