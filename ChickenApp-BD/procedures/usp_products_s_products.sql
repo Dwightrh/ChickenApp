d1f7ae8bdbcdaa9155c61227b457ced57186a359
dwight <dromero551@gmail.com>
2020-12-13 16:51:58 -0500

    Delete usp_products_s_products.sql

--- ChickenApp-BD/procedures/usp_products_s_products.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`admin`@`%` PROCEDURE `usp_products_s_products`()
-BEGIN
-	IF (SELECT EXISTS(SELECT * FROM Products)) THEN
-		SELECT idProducts AS "idProduct" , nameProducts AS "name" , descriptionProducts AS "description" ,
-		priceProducts AS "price" , Categories_idCategories AS "categoryid" 
-		FROM Products where estateProducts=1;
-    END IF;
-END
