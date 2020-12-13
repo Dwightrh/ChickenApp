17f41fa9a6886807d1388c4838efe3db7bcde3a4
dwight <dromero551@gmail.com>
2020-12-13 16:52:10 -0500

    Delete usp_product_f_product.sql

--- ChickenApp-BD/procedures/usp_product_f_product.sql
+++
@@ -1,9 +0,0 @@
-CREATE DEFINER=`admin`@`%` PROCEDURE `usp_product_d_product`(in pproductid int,out oresult int)
-BEGIN
-	IF (SELECT EXISTS(SELECT * FROM Products WHERE idProducts=pproductid AND estateProducts=1)) THEN
-		UPDATE Products 
-		SET estateProducts=0 
-		WHERE idProducts=pproductid;
-		SET oresult=1;
-	END IF;
-END
