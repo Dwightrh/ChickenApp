58d3138550791e07b72cf2412fae7c6c3c2c9bda
dwight <dromero551@gmail.com>
2020-12-13 09:30:49 -0500

    Update usp_categoria_i_categoria.sql

--- ChickenApp-BD/procedures/usp_categoria_i_categoria.sql
+++ ChickenApp-BD/procedures/usp_categoria_i_categoria.sql
@@ -1,16 +1,19 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `usp_categoria_i_categoria`(out id int,in nombre varchar(45) ,in descripcion varchar(200))
 BEGIN
-	if (SELECT EXISTS(select * from categories where nameCategories=nombre))=false then
-		insert into categories (nameCategories,descriptionCategories,estateCategories) values (nombre,descripcion,1);
-		if (SELECT EXISTS(select * from categories where nameCategories=nombre and descriptionCategories=descripcion and estateCategories=1)) then
-			select 'Se realizo la operacion';
-            select idCategories into id from categories where nameCategories=nombre and descriptionCategories=descripcion and estateCategories=1;
-        else
-			select 'No se realizo la operacion';
-            set id=-1;
-        end if;
-	else	
-		select 'Ya existe esta categoria';
-        select idCategories into id from categories where nameCategories=nombre;
-	end if;
+	IF (SELECT EXISTS(SELECT * FROM categories WHERE nameCategories=nombre))=false THEN
+		INSERT INTO categories (nameCategories,descriptionCategories,estateCategories) 
+		VALUES (nombre,descripcion,1);
+		IF (SELECT EXISTS(SELECT * FROM categories WHERE nameCategories=nombre 
+		AND descriptionCategories=descripcion AND estateCategories=1)) THEN
+			SELECT 'Se realizo la operacion';
+            SELECT idCategories INTO id FROM categories WHERE nameCategories=nombre 
+			AND descriptionCategories=descripcion AND estateCategories=1;
+        ELSE
+			SELECT 'No se realizo la operacion';
+            SET id=-1;
+        END IF;
+	ELSE	
+		SELECT 'Ya existe esta categoria';
+        SELECT idCategories INTO id FROM categories WHERE nameCategories=nombre;
+	END IF;
 END
