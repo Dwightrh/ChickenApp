6816200fb9e3e5d084e0413210f2a43ff4f01013
dwight <dromero551@gmail.com>
2020-12-13 16:51:57 -0500

    Delete usp_product_i_product.sql

--- ChickenApp-BD/procedures/usp_product_i_product.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`admin`@`%` PROCEDURE `usp_product_i_product`(in pname varchar(45),in pdescription varchar(200),in pprice varchar(150),in  pcategoryid int,out oresult int)
-BEGIN
-	IF (SELECT EXISTS(SELECT * FROM Products WHERE nameProducts=pname))=false THEN
-		INSERT INTO Products (nameProducts,descriptionProducts,priceProducts,Categories_idCategories,estateProducts) 
-		VALUES (pname,pdescription,pprice,pcategoryid,1);
-		SET oresult=1;
-	END IF;
-END
