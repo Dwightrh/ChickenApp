bc3b0e9a12576fbdc472fe555c7f7899f9abcafd
dwight <dromero551@gmail.com>
2020-12-13 17:00:13 -0500

    Delete product-get-response.model.ts

--- chickenAppFront/src/app/product/models/product-get-response.model.ts
+++
@@ -1,8 +0,0 @@
-// modelo segun la respuesta que envia el backend por GET
-export class ProductGetResponse {
-    status: boolean ;
-    statusCode: number ;
-    message: string ;
-    product: any ;
-  }
-  
