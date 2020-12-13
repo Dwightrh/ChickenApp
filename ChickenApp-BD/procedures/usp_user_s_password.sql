89584ae828fd344802f563540387a635686148e6
dwight <dromero551@gmail.com>
2020-12-13 09:59:42 -0500

    Update usp_user_s_password.sql

--- ChickenApp-BD/procedures/usp_user_s_password.sql
+++ ChickenApp-BD/procedures/usp_user_s_password.sql
@@ -1,6 +1,4 @@
 CREATE DEFINER=`admin`@`%` PROCEDURE `usp_user_s_password`(in pemail varchar(45),out oresult varchar(255))
 BEGIN
-
-     set oresult=(select passwordUsers from Users where emailusers=pemail);
-     
+     SET oresult=(SELECT passwordUsers FROM Users WHERE emailusers=pemail);  
 END
