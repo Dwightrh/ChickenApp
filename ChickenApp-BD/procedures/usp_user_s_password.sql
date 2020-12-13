86657677ef7b0ff17f09da6bff1da6806a70eb00
dwight <dromero551@gmail.com>
2020-12-13 16:52:02 -0500

    Delete usp_user_s_password.sql

--- ChickenApp-BD/procedures/usp_user_s_password.sql
+++
@@ -1,4 +0,0 @@
-CREATE DEFINER=`admin`@`%` PROCEDURE `usp_user_s_password`(in pemail varchar(45),out oresult varchar(255))
-BEGIN
-     SET oresult=(SELECT passwordUsers FROM Users WHERE emailusers=pemail);  
-END
